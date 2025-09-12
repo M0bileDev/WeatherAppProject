d0f930cbba3389162f5bff4198c93707a0f17d83
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-12 21:23:20 +0200

    Create model of network response;

---
+++ app/src/main/java/com/example/data/remote/HourlyUnits.kt
@@ -0,0 +1,21 @@
+package com.example.data.remote
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class HourlyUnits(
+    @param:Json(name = "pressure_msl")
+    val pressureMsl: String,
+    @param:Json(name = "relativehumidity_2m")
+    val relativeHumidity2m: String,
+    @param:Json(name = "temperature_2m")
+    val temperature2m: String,
+    @param:Json(name = "time")
+    val time: String,
+    @param:Json(name = "weathercode")
+    val weatherCode: String,
+    @param:Json(name = "windspeed_10m")
+    val windSpeed10m: String
+)
