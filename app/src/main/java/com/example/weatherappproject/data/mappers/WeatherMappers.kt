a5a8885428c3f26d3c5399af4c8dba786c6145d1
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-12 22:38:26 +0200

    Create mappers from dto to domain;

--- app/src/main/java/com/example/weatherappproject/data/mappers/WeatherMappers.kt
+++ app/src/main/java/com/example/weatherappproject/data/mappers/WeatherMappers.kt
@@ -1,6 +1,56 @@
 package com.example.weatherappproject.data.mappers
 
+import com.example.weatherappproject.data.mappers.model.IndexedWeatherData
+import com.example.weatherappproject.data.remote.model.Hourly
 import com.example.weatherappproject.data.remote.model.WeatherDto
+import com.example.weatherappproject.domain.weather.WeatherData
+import com.example.weatherappproject.domain.weather.WeatherInfo
+import com.example.weatherappproject.domain.weather.WeatherType
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter.ISO_DATE_TIME
 
 //Mappers from dto to domain model
+
+fun Hourly.toWeatherDataMap(): Map<Int, List<WeatherData>> {
+    return time.mapIndexed { index, string ->
+        val temperature = temperature2m[index]
+        val weatherCode = weatherCode[index]
+        val windSpeed = windSpeed10m[index]
+        val pressure = pressureMsl[index]
+        val humidity = relativeHumidity2m[index]
+
+        val weatherData = WeatherData(
+            time = LocalDateTime.parse(string, ISO_DATE_TIME),
+            temperatureCelsius = temperature,
+            pressure = pressure,
+            windSpeed = windSpeed,
+            humidity = humidity,
+            weatherType = WeatherType.fromWMO(weatherCode)
+        )
+        //Indexed from 0 to 167 as hour from 0 to 23, for next days (max 7 days ahead)
+        IndexedWeatherData(index, weatherData = weatherData)
+    }
+        //Want to group WeatherData for each day -> index 0 means current day [hours: 0, 1, 2 ,3...,],
+        // next day [hours: 0, 1, 2 ,3...,] etc day+n (n<7)
+        .groupBy {
+            it.index % 7
+        }
+        //Indexed with IndexedWeatherData but WeatherData needed so extract WeatherData from IndexedWeatherData
+        .mapValues { values ->
+            values.value.map { it.weatherData }
+        }
+
+}
+
+fun WeatherDto.toWeatherInfo(): WeatherInfo {
+    val weatherDataMap = hourly.toWeatherDataMap()
+    val now = LocalDateTime.now()
+    val currentWeatherData = weatherDataMap[0]?.find { weatherData ->
+        weatherData.time.hour == now.hour
+    }
+    return WeatherInfo(
+        weatherDataPerDay = weatherDataMap,
+        currentWeatherData = currentWeatherData
+    )
+}
 
