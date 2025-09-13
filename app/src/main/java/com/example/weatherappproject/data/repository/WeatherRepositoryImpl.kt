fa6c8ff37a82b77652e18ee956706435686ab724
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-13 19:37:08 +0200

    Create impl of weather repo;

---
+++ app/src/main/java/com/example/weatherappproject/data/repository/WeatherRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.example.weatherappproject.data.repository
+
+import com.example.weatherappproject.data.mappers.toWeatherInfo
+import com.example.weatherappproject.data.remote.api.WeatherApi
+import com.example.weatherappproject.domain.repository.WeatherRepository
+import com.example.weatherappproject.domain.util.Resource
+import com.example.weatherappproject.domain.weather.WeatherInfo
+import javax.inject.Inject
+
+class WeatherRepositoryImpl @Inject constructor(
+    private val weatherApi: WeatherApi
+) : WeatherRepository {
+    override suspend fun getWeather(
+        lat: Double,
+        long: Double
+    ): Resource<WeatherInfo> {
+        return try {
+            val data = weatherApi.getWeather(lat, long).run { toWeatherInfo() }
+            Resource.Success(data)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Resource.Error(e.localizedMessage ?: "An unknown error occurred")
+        }
+    }
+
+}
