b3c6391eeb64fe462f37c2d784fbe214155f5112
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-14 00:43:33 +0200

    continue hilt setup; config main activity;

--- app/src/main/java/com/example/weatherappproject/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherappproject/presentation/MainActivity.kt
@@ -11,8 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.weatherappproject.BuildConfig
 import com.example.weatherappproject.presentation.ui.theme.WeatherAppProjectTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
