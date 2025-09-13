a2cf6b9e37e7e7ee77be6766508a221898735c2c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-14 00:42:31 +0200

    create data specific repo;

---
+++ app/src/main/java/com/example/weatherappproject/data/repository/DefaultWeatherRepository.kt
@@ -0,0 +1,5 @@
+package com.example.weatherappproject.data.repository
+
+import com.example.weatherappproject.domain.repository.WeatherRepository
+
+interface DefaultWeatherRepository : WeatherRepository
