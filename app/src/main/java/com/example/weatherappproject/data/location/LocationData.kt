9d71f4f498f6df78dc0aaeb2e49092861bd3aa36
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-14 21:06:27 +0200

    Change package;

--- app/src/main/java/com/example/weatherappproject/data/location/LocationData.kt
+++ app/src/main/java/com/example/weatherappproject/data/location/LocationData.kt
@@ -1,6 +1,6 @@
 package com.example.weatherappproject.data.location
 
-import com.example.weatherappproject.domain.location.model.LocationData
+import com.example.weatherappproject.domain.model.location.LocationData
 
 data class LocationData(
     val lat: Double,
