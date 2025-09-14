d0c13103e56fc9662f2071d05dc704f3131d38c6
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-14 21:06:39 +0200

    Change package;

--- app/src/main/java/com/example/weatherappproject/domain/location/LocationTracker.kt
+++ app/src/main/java/com/example/weatherappproject/domain/location/LocationTracker.kt
@@ -1,6 +1,6 @@
 package com.example.weatherappproject.domain.location
 
-import com.example.weatherappproject.domain.location.model.LocationData
+import com.example.weatherappproject.domain.model.location.LocationData
 
 interface LocationTracker {
     suspend fun  getCurrentLocation() : LocationData?
