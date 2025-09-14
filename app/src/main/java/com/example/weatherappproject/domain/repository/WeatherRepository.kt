1ae06e5d45c55bb81bc74c9115f30efc1ce83e11
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-14 21:08:29 +0200

    Change package;

--- app/src/main/java/com/example/weatherappproject/domain/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherappproject/domain/repository/WeatherRepository.kt
@@ -1,7 +1,7 @@
 package com.example.weatherappproject.domain.repository
 
-import com.example.weatherappproject.domain.util.Resource
-import com.example.weatherappproject.domain.weather.WeatherInfo
+import com.example.weatherappproject.domain.model.utils.Resource
+import com.example.weatherappproject.domain.model.weather.WeatherInfo
 
 interface WeatherRepository {
     suspend fun getWeather(lat: Double, long: Double): Resource<WeatherInfo>
