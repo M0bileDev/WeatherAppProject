57333c3f87312e2fdb736a394268e1df05968421
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-14 21:06:22 +0200

    Change package;

--- app/src/main/java/com/example/weatherappproject/data/mappers/model/IndexedWeatherData.kt
+++ app/src/main/java/com/example/weatherappproject/data/mappers/model/IndexedWeatherData.kt
@@ -1,6 +1,7 @@
 package com.example.weatherappproject.data.mappers.model
 
-import com.example.weatherappproject.domain.weather.WeatherData
+import com.example.weatherappproject.data.model.weather.WeatherData
+
 
 data class IndexedWeatherData(
     val index: Int,
