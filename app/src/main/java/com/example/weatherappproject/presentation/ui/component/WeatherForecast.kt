8fea0637e191ad7b197a00a370e2294914ba6a3c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-16 01:05:13 +0200

    create ui component for list;

---
+++ app/src/main/java/com/example/weatherappproject/presentation/ui/component/WeatherForecast.kt
@@ -0,0 +1,40 @@
+package com.example.weatherappproject.presentation.ui.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherappproject.presentation.model.WeatherDataPresentation
+
+@Composable
+fun WeatherForecast(
+    modifier: Modifier = Modifier,
+    weatherDataForToday: List<WeatherDataPresentation>
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(text = "Today", fontSize = 20.sp)
+        Spacer(Modifier.height(16.dp))
+        LazyRow {
+            items(weatherDataForToday) { presentation ->
+                HourlyWeatherDisplay(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .padding(end = 16.dp),
+                    weatherDataPresentation = presentation
+                )
+            }
+        }
+    }
+}
