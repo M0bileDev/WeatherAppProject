c7db5221372e044f6433c446d61a3b898b467c53
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-15 14:06:28 +0200

    add logic to handle permissions; update view to display weather card;

--- app/src/main/java/com/example/weatherappproject/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherappproject/presentation/MainActivity.kt
@@ -1,50 +1,64 @@
 package com.example.weatherappproject.presentation
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.weatherappproject.BuildConfig
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.weatherappproject.presentation.ui.component.WeatherCard
 import com.example.weatherappproject.presentation.ui.theme.WeatherAppProjectTheme
+import com.example.weatherappproject.presentation.viewmodel.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: WeatherViewModel by viewModels()
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            viewModel.loadWeatherInfo()
+        }.also { launcher ->
+            launcher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                )
+            )
+        }
+
+
         enableEdgeToEdge()
         setContent {
             WeatherAppProjectTheme {
+
+                val state by viewModel.state.collectAsStateWithLifecycle()
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Column(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        WeatherCard(
+                            state = state
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherAppProjectTheme {
-        Greeting("Android")
     }
 }
