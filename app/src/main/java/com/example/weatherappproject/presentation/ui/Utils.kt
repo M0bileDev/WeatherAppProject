faf5ce361e956017a0eb3030f92e981c0fe933e4
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-16 01:03:54 +0200

    create lazy of date formatter;

---
+++ app/src/main/java/com/example/weatherappproject/presentation/ui/Utils.kt
@@ -0,0 +1,8 @@
+package com.example.weatherappproject.presentation.ui
+
+import java.time.format.DateTimeFormatter
+
+
+val dateFormatterHHmm: DateTimeFormatter? by lazy {
+    DateTimeFormatter.ofPattern("HH:mm")
+}
