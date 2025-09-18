edc94ad70681ff0e4eaac656c2afcfe645407249
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-19 01:04:50 +0200

    set derived type;

--- app/src/main/java/com/example/weatherappproject/data/repository/DefaultWeatherRepository.kt
+++ app/src/main/java/com/example/weatherappproject/data/repository/DefaultWeatherRepository.kt
@@ -1,5 +1,6 @@
 package com.example.weatherappproject.data.repository
 
+import com.example.weatherappproject.data.model.weather.WeatherInfo
 import com.example.weatherappproject.domain.repository.WeatherRepository
 
-interface DefaultWeatherRepository : WeatherRepository
+interface DefaultWeatherRepository : WeatherRepository<WeatherInfo>
