245fae51dec49a2bdf0aaf6dcf55e6e9b4d013c0
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-19 01:04:37 +0200

    set upper bound;

--- app/src/main/java/com/example/weatherappproject/domain/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherappproject/domain/repository/WeatherRepository.kt
@@ -3,6 +3,6 @@ package com.example.weatherappproject.domain.repository
 import com.example.weatherappproject.utils.Resource
 import com.example.weatherappproject.domain.model.weather.WeatherInfoDomain
 
-interface WeatherRepository {
-    suspend fun getWeather(lat: Double, long: Double): Resource<WeatherInfoDomain>
+interface WeatherRepository<T: WeatherInfoDomain> {
+    suspend fun getWeather(lat: Double, long: Double): Resource<T>
 }
