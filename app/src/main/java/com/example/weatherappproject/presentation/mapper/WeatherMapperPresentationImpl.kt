d5589f323bb3f62c54c60325842cce7c1dcb4c43
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-19 01:44:20 +0200

    provide implementation of mapper;

---
+++ app/src/main/java/com/example/weatherappproject/presentation/mapper/WeatherMapperPresentationImpl.kt
@@ -0,0 +1,42 @@
+package com.example.weatherappproject.presentation.mapper
+
+import com.example.weatherappproject.data.model.weather.WeatherInfoData
+import com.example.weatherappproject.presentation.model.WeatherDataPresentation
+import com.example.weatherappproject.presentation.model.WeatherInfoPresentation
+import com.example.weatherappproject.presentation.model.WeatherType.Companion.fromWMO
+import javax.inject.Inject
+
+class WeatherMapperPresentationImpl @Inject constructor() : WeatherMapperPresentation {
+    override fun WeatherInfoData.toWeatherDataPresentation(): WeatherInfoPresentation {
+
+        val weatherDataPerDay = weatherPerDay.mapValues {
+            it.value.map { weatherData ->
+                WeatherDataPresentation(
+                    weatherData.time,
+                    weatherData.temperatureCelsius,
+                    weatherData.pressure,
+                    weatherData.windSpeed,
+                    weatherData.humidity,
+                    fromWMO(weatherData.weatherCode)
+                )
+            }.sortedBy { timeSort -> timeSort.timeData.hour }
+        }
+
+        val weatherData = currentWeather?.let {
+            WeatherDataPresentation(
+                it.time,
+                it.temperatureCelsius,
+                it.pressure,
+                it.windSpeed,
+                it.humidity,
+                fromWMO(it.weatherCode)
+            )
+        }
+
+        return WeatherInfoPresentation(
+            weatherDataPerDay,
+            weatherData
+        )
+    }
+}
+
