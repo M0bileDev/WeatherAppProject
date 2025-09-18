f0764c52891351e024987cd057f04f03386c688b
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-19 00:55:52 +0200

    correct misspell;

--- app/src/main/java/com/example/weatherappproject/domain/model/weather/WeatherInfo.kt
+++ app/src/main/java/com/example/weatherappproject/domain/model/weather/WeatherInfo.kt
@@ -1,7 +1,7 @@
 package com.example.weatherappproject.domain.model.weather
 
 abstract class WeatherInfo(
-    // key - index of weekly representation of day 0-tody, 1-tomorrow etc
+    // key - index of weekly representation of day 0-today, 1-tomorrow etc
     // value - weather data that contains info about temperature, pressure etc.
     open val weatherDomainPerDay: Map<Int, List<WeatherDomain>>,
     // representation of time-specific weather -> request data at 1pm, this value will
