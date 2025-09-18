74f2a95f4f35f528c49ba91cdcab694d9d6528cf
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-19 01:11:21 +0200

    set argument type;

--- app/src/main/java/com/example/weatherappproject/data/location/DefaultLocationTracker.kt
+++ app/src/main/java/com/example/weatherappproject/data/location/DefaultLocationTracker.kt
@@ -2,4 +2,4 @@ package com.example.weatherappproject.data.location
 
 import com.example.weatherappproject.domain.location.LocationTracker
 
-interface DefaultLocationTracker : LocationTracker
+interface DefaultLocationTracker : LocationTracker<LocationData>
