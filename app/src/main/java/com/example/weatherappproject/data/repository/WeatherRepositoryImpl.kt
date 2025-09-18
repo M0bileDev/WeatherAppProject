bded2a9981306b6aeb2a087c2754c5e17f9c08e8
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-19 01:24:07 +0200

    inject mapper;

--- app/src/main/java/com/example/weatherappproject/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/weatherappproject/data/repository/WeatherRepositoryImpl.kt
@@ -1,20 +1,25 @@
 package com.example.weatherappproject.data.repository
 
-import com.example.weatherappproject.data.mappers.toWeatherInfo
+import com.example.weatherappproject.data.mappers.WeatherMapper
 import com.example.weatherappproject.data.model.weather.WeatherInfoData
 import com.example.weatherappproject.data.remote.api.WeatherApi
 import com.example.weatherappproject.utils.Resource
 import javax.inject.Inject
 
 class WeatherRepositoryImpl @Inject constructor(
-    private val weatherApi: WeatherApi
+    private val weatherApi: WeatherApi,
+    private val weatherMapper: WeatherMapper
 ) : DefaultWeatherRepository {
     override suspend fun getWeather(
         lat: Double,
         long: Double
     ): Resource<WeatherInfoData> {
         return try {
-            val data = weatherApi.getWeather(lat, long).run { toWeatherInfo() }
+            val data = weatherApi.getWeather(lat, long).run {
+                with(weatherMapper) {
+                    toWeatherInfo()
+                }
+            }
             Resource.Success(data)
         } catch (e: Exception) {
             e.printStackTrace()
