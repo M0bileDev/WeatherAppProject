401afd08a4597ebb3633c6b5588fd335a66915fa
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-19 21:45:06 +0200

    update view;

--- app/src/main/java/com/example/weatherappproject/presentation/ui/component/HourlyWeatherDisplay.kt
+++ app/src/main/java/com/example/weatherappproject/presentation/ui/component/HourlyWeatherDisplay.kt
@@ -9,8 +9,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalResources
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.weatherappproject.R
 import com.example.weatherappproject.presentation.model.WeatherPresentation
 import com.example.weatherappproject.presentation.ui.dateFormatterHHmm
 
@@ -18,24 +21,28 @@ import com.example.weatherappproject.presentation.ui.dateFormatterHHmm
 fun HourlyWeatherDisplay(
     modifier: Modifier = Modifier,
     weatherPresentation: WeatherPresentation
-) {
+) = with(weatherPresentation) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-        val time = remember(weatherPresentation.timeData) {
-            weatherPresentation.timeData.format(
+        val time = remember(timeData) {
+            timeData.format(
                 dateFormatterHHmm
             )
-        } ?: "No data"
+        } ?: stringResource(R.string.no_data)
         Text(text = time)
         Image(
             modifier = Modifier.size(40.dp),
-            painter = painterResource(weatherPresentation.weatherType.iconRes),
-            contentDescription = weatherPresentation.weatherType.weatherDesc,
+            painter = painterResource(weatherType.iconRes),
+            contentDescription = weatherType.weatherDesc,
         )
-        Text(text = "${weatherPresentation.temperatureCelsiusData}°C")
+        Text(
+            text = stringResource(
+                R.string.temperature_x_celsius,
+                temperatureCelsiusData
+            )
+        )
     }
-
 }
