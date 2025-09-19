7b971213ebb2fa33f725baa6862244ee7c0dc2c5
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-19 21:55:03 +0200

    reformat line;

--- app/src/main/java/com/example/weatherappproject/presentation/ui/Utils.kt
+++ app/src/main/java/com/example/weatherappproject/presentation/ui/Utils.kt
@@ -2,7 +2,6 @@ package com.example.weatherappproject.presentation.ui
 
 import java.time.format.DateTimeFormatter
 
-
 val dateFormatterHHmm: DateTimeFormatter? by lazy {
     DateTimeFormatter.ofPattern("HH:mm")
 }
