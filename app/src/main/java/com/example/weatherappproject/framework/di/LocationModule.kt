e5611c3d5636e1409f5e9228bc289e51097db331
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-21 01:26:55 +0200

    divide module for binder and provider; sadly it's not possible to handle provider and binder in the same module, thanks Dagger Hilt :);

--- app/src/main/java/com/example/weatherappproject/framework/di/LocationModule.kt
+++ app/src/main/java/com/example/weatherappproject/framework/di/LocationModule.kt
@@ -1,17 +1,32 @@
 package com.example.weatherappproject.framework.di
 
+import android.content.Context
 import com.example.weatherappproject.data.location.DefaultLocationTracker
 import com.example.weatherappproject.framework.location.DefaultLocationTrackerImpl
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @[Module InstallIn(SingletonComponent::class)]
-abstract class LocationModule {
+abstract class LocationModuleBinder {
 
     @[Binds Singleton]
     abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTrackerImpl): DefaultLocationTracker
 
+
+}
+
+@[Module InstallIn(SingletonComponent::class)]
+object LocationModuleProvider {
+
+    @[Provides Singleton]
+    fun provideFusedLocationProviderClient(
+        @ApplicationContext context: Context
+    ): FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
 }
