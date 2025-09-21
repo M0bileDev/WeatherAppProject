24645895df557b35fce20a9538310fbe9ea0b83f
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-22 00:30:33 +0200

    create test;

--- app/src/test/java/com/example/weatherappproject/presentation/viewmodel/WeatherViewModelTest.kt
+++ app/src/test/java/com/example/weatherappproject/presentation/viewmodel/WeatherViewModelTest.kt
@@ -3,8 +3,14 @@ package com.example.weatherappproject.presentation.viewmodel
 import com.example.weatherappproject.data.location.DefaultLocationTracker
 import com.example.weatherappproject.data.repository.DefaultWeatherRepository
 import com.example.weatherappproject.presentation.mapper.WeatherMapperPresentation
+import com.example.weatherappproject.presentation.model.WeatherViewModelAction
+import io.mockk.coEvery
 import io.mockk.mockk
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 
 class WeatherViewModelTest {
 
@@ -22,5 +28,19 @@ class WeatherViewModelTest {
             weatherMapperPresentation
         )
     }
+
+    @Test
+    fun givenViewModel_whenLocationIsNull_thenWeatherInfoStateIsNull() {
+        //given viewModel
+
+        //when current location is null
+        coEvery { locationTracker.getCurrentLocation() } returns null
+        weatherViewModel.loadWeatherInfo()
+
+        //then
+        assertEquals(weatherViewModel.state.value.weatherInfo, null)
+    }
+
+
 
 }
