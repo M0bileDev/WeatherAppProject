4fe3e0e10eaef387dcf98d30d0a6421b29b17c0c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-09-22 11:40:14 +0200

    introduce dispatcher provider (abstraction for testing);

---
+++ app/src/main/java/com/example/weatherappproject/framework/dispatcher/DispatcherProvider.kt
@@ -0,0 +1,9 @@
+package com.example.weatherappproject.framework.dispatcher
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+interface DispatcherProvider {
+    val io: CoroutineDispatcher
+    val main: CoroutineDispatcher
+    val default: CoroutineDispatcher
+}
